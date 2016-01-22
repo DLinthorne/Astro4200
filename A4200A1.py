fb6624c77171f6b18b41da6226f7156e55f912b7
Nick <nicholas.layden@gmail.com>
2016-01-22 19:30:39 -0400

    files

---
+++ A4200A1.py
@@ -0,0 +1,44 @@
+
+import astropy as ap
+from astropy.io import fits
+import os
+
+
+
+Directory = ('/home/nick/Desktop/School/Astro4200/MZCYG')
+
+''' LIST OF ALL OF THE STARS IN THE DIRECTORY THAT ARE FITS FILES. '''
+files = []
+for file in os.listdir(Directory):
+    if file.endswith('.fit'):
+        files.append(file)
+
+
+
+''' This line opens one of the fits files into memory. To observe its contituents, we need to use 
+    the library functions to continue. '''
+Star = fits.open('{}/{}'.format(Directory,files[0]))
+
+
+
+Starnames = []
+for i in range(len(files)):
+    Starnames.append(files[i].split('.'))
+    
+    
+    
+'''Uses the command fits2bitmap in the system terminal to change all fits files into png images. 
+   DO THIS ONLY ONE TIME, THEN COMMENT IT OUT!
+   UNTESTED ON MACOS, if it works, OMG NICE. IF not, well theres probably a mac terminal command to do the
+   same thing. In that case, replace the argument in the brackets with the mac command. 
+'''
+#for i in range(len(files)):
+#    os.system('fits2bitmap {} -o {}'.format(Directory+'/'+files[i], Directory+'/'+Starnames[i][0]))
+
+
+
+
+
+    
+
+
